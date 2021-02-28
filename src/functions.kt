23efcfcc0a6087b3ce2ee0867b0949b5fa062f72
stella4545 <stellachepchumbah@gmail.com>
2021-02-28 16:34:46 +0300

    functions

---
+++ src/functions.kt
@@ -0,0 +1,26 @@
+fun main(){
+    var name:String=("hello Stella");
+    println(name)
+
+    var module=(100%20)
+    println(module)
+
+    var addition=(10+20+60+40)
+    println(addition)
+
+    var facts:String=("Iam a down to earth person and very optimistic")
+    println(facts)
+
+}
+fun modulus(num1:Int,num2:Int) :Int{
+    var module=(num1%num2)
+    return(module)
+    }
+
+    fun addition(num1:Int,num2: Int,num3:Int,num4:Int):Int{
+        var addition =(num1+num2+num3+num4);Int;
+        return addition
+
+    }
+
+
